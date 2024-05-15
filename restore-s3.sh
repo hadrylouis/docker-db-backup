e74d0b5491775693c048ffbcfbee3a68998453db
Hadrien Louis <hadry@protonmail.com>
2024-05-15 07:37:14 +0200

    Improve restore script

--- restore-s3.sh
+++ restore-s3.sh
@@ -4,39 +4,65 @@ export AWS_ACCESS_KEY_ID=${DEFAULT_S3_KEY_ID}
 export AWS_SECRET_ACCESS_KEY=${DEFAULT_S3_KEY_SECRET}
 export AWS_DEFAULT_REGION=${DEFAULT_S3_REGION}
 export PARAMS_AWS_ENDPOINT_URL=" --endpoint-url ${DEFAULT_S3_PROTOCOL}://${DEFAULT_S3_HOST}"
-export SOURCE_FILE="pgsql_.*\.sql.zst$"
 export TEMP_PATH="/tmp"
 
-# List all backup files
-echo "Voici tous les fichiers de sauvegarde disponibles :"
-aws s3 ls s3://${DEFAULT_S3_BUCKET}/${DEFAULT_S3_PATH}/ ${PARAMS_AWS_ENDPOINT_URL} --recursive | grep -E ${SOURCE_FILE} | awk '{print $4}'
+# Check connection to S3
+if ! aws s3 ls ${PARAMS_AWS_ENDPOINT_URL} > /dev/null 2>&1; then
+    echo -e "\nError: Failed to connect to S3. Please check your connection and try again."
+    exit 1
+fi
+# Filter files from S3
+if [ "${DB01_COMPRESSION}" == "zstd" ]; then
+    export SOURCE_FILE=".*\.sql.zst$"
+else
+    export SOURCE_FILE=".*\.sql.${DB01_COMPRESSION}$"
+fi
 
-# Ask the user to choose a file
-echo "Veuillez entrer le nom du fichier que vous souhaitez restaurer :"
+echo -e "--------------------------------------------------"
+echo -e "List of all available backup files:"
+echo -e "--------------------------------------------------"
+
+BACKUP_FILES=$(aws s3 ls s3://${DEFAULT_S3_BUCKET}/${DEFAULT_S3_PATH}/ ${PARAMS_AWS_ENDPOINT_URL} --recursive | grep -E ${SOURCE_FILE} | awk '{print $4}')
+echo "${BACKUP_FILES}"
+
+echo -e "\n--------------------------------------------------"
+echo -e "Please enter the name of the file you wish to restore:"
+echo -e "--------------------------------------------------"
+echo -n "> "
 read CHOSEN_FILE
 
-# Set the chosen file as the latest file
-LATEST_FILE=$CHOSEN_FILE
+# Check if the chosen file exists
+if echo "${BACKUP_FILES}" | grep -q "${CHOSEN_FILE}"; then
+    LATEST_FILE=$CHOSEN_FILE
+else
+    echo -e "\nError: The chosen file does not exist. Please try again."
+    exit 1
+fi
 
-# Display a summary of the restore parameters
-echo "Vous avez choisi de restaurer le fichier suivant : $LATEST_FILE"
+echo -e "\n--------------------------------------------------"
+echo -e "Restoration Summary:"
+echo -e "--------------------------------------------------"
 
-# Display a detailed summary of the restore parameters
-echo "Résumé de la restauration :"
-echo "Fichier à restaurer : $LATEST_FILE"
-echo "Emplacement de la restauration : s3://${DEFAULT_S3_BUCKET}/${DEFAULT_S3_PATH}/"
-echo "Base de données de destination : ${DB01_NAME} sur ${DB01_HOST}:${DB01_PORT}"
-
-# Ask the user for confirmation
-read -p "Avec ces paramètres, êtes-vous sûr de vouloir continuer ? (y/n) " -n 1 -r
+echo -e "File to restore: $LATEST_FILE"
+echo -e "Restoration location: s3://${DEFAULT_S3_BUCKET}/${DEFAULT_S3_PATH}/"
+echo -e "Destination database:"
+echo -e "\t - Host: ${DB01_HOST}:${DB01_PORT}"
+echo -e "\t - DB Name: ${DB01_NAME}"
+echo -e ""
+echo -e "With these parameters, are you sure you want to continue? (y/n) "
+echo -n "> "
+read -p "" -n 1 -r
 
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    # Proceed with the restore
-    echo "Restauration en cours..."
+    echo -e "\n--------------------------------------------------"
+    echo -e "Restoration in progress..."
+    echo -e "--------------------------------------------------"
     aws s3 cp s3://${DEFAULT_S3_BUCKET}/${LATEST_FILE} ${TEMP_PATH}/${LATEST_FILE} ${PARAMS_AWS_ENDPOINT_URL}
     restore ${TEMP_PATH}/${LATEST_FILE} ${DB01_TYPE} ${DB01_HOST} ${DB01_NAME} ${DB01_USER} ${DB01_PASS} ${DB01_PORT}
 else
-    echo "Restauration annulée."
+    echo -e "\n--------------------------------------------------"
+    echo -e "Restoration cancelled."
+    echo -e "--------------------------------------------------"
 fi
